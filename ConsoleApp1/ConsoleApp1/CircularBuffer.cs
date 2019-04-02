0fd095d8bb56ed38e049457e5e10c4980587be69
Michal Matis <miso.matis@gmail.com>
2019-04-02 13:42:14 +0200

    inputs not secured but working

---
+++ ConsoleApp1/ConsoleApp1/CircularBuffer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class CircularBuffer
+    {
+        private const int bufferSize = 3;
+        private  char[] _buffer = new char[bufferSize];
+        private int variable = 0;
+
+        public bool Push(char input)
+        {
+            if (variable >= bufferSize)
+            {
+                return false;
+            }
+            else
+            {
+                _buffer[variable] = input;
+                variable++;
+                return true;
+            }
+
+        }
+
+        public void WriteBuffer()
+        {
+            foreach (var something in _buffer)
+            {
+                Console.WriteLine(something);
+            }
+        }
+
+        public bool ForcePush(char data)
+        {
+            _buffer[variable%bufferSize] = data;
+            variable++;
+            return true;
+        }
+
+        public char Pop()
+        {
+            if (variable != 0)
+            {
+                variable--;
+            }
+            char result = _buffer[variable % bufferSize];
+            _buffer[variable % bufferSize] =' ';
+          
+            return result;
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularBuffer buffer = new CircularBuffer();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1:Push\n" +
+                    "2:Force Push\n" +
+                    "3:Pop\n" +
+                    "4:Write\n");
+
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter char to push");
+                            if (buffer.Push(Convert.ToChar(Console.ReadLine())))
+                            {
+                                Console.WriteLine("truee");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("False");
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter char to Forcepush");
+                            buffer.ForcePush(Convert.ToChar(Console.ReadLine()));
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine($"last char is:{buffer.Pop()}");
+                            
+                            break;
+                        }
+                    case 4:
+                        {
+                            buffer.WriteBuffer();
+                            break;
+                        }
+
+                    default:
+                        {
+                            break;
+                        }
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
